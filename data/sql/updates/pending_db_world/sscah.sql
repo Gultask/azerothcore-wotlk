efab171912be5870e9a0ed20f6e06c096ef0bcd3
Gultask <100873791+Gultask@users.noreply.github.com>
2023-12-09 13:06:19 -0300

    Create sscah.sql

---
+++ data/sql/updates/pending_db_world/sscah.sql
@@ -0,0 +1,4 @@
+SET @CGUID := 153000;
+UPDATE `creature` SET `comment` = 'Positions were changed to allow previous script logic to function, Sniffed: (X: -239.715, Y: -366.44, Z: -0.744514, O: 1.23918)' WHERE `guid` = @CGUID+19;
+UPDATE `creature` SET `comment` = 'Positions were changed to allow previous script logic to function, Sniffed: (X: -215.753, Y: -375.343, Z: 38.403, O: 5.00909)' WHERE `guid` = @CGUID+20;
+UPDATE `creature` SET `comment` = 'Positions were changed to allow previous script logic to function, Sniffed: (X: -264.165, Y: -357.171, Z: 38.8069, O: 2.84489)' WHERE `guid` = @CGUID+21;
